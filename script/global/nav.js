f0cf649295ef4807d08b5942670ae53f6df3b38e
Ricki Beckless <glbeckless@hotmail.com>
2024-12-01 16:26:24 -0600

    fix: update active class for home link

--- script/global/nav.js
+++ script/global/nav.js
@@ -4,7 +4,12 @@ async function checkIfActive() {
     let path = window.location.pathname;
     let links = document.querySelectorAll('.main-navbar-item a');
     links.forEach(link => {
-        if (link.href.includes(path)) {
+        // if link is only /, then it is the home page
+        if (link.href === import.meta.PRO_URL) {
+            if (path === '/') {
+                link.classList.add('active');
+            }
+        } else if (link.href.includes(path)) {
             link.classList.add('active');
         }
     });
