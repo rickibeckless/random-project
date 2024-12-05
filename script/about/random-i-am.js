36e1ab14403c0a39bbf0d6957ee8689398f24d25
Ricki Beckless <glbeckless@hotmail.com>
2024-12-04 23:55:30 -0600

    begin about page

--- script/about/random-i-am.js
+++ script/about/random-i-am.js
@@ -9,6 +9,10 @@ async function handleIAmChange() {
     const iAmData = await fetchIAm();
     let i = 0;
     setInterval(() => {
+        iAm.classList.remove('fade-in-up');
+        void iAm.offsetWidth;
+        iAm.classList.add('fade-in-up');
+
         iAm.textContent = iAmData[i];
         i = (i + 1) % iAmData.length;
     }, 1500);
